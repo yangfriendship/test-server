02e17a5c2d80e1568c5e38def4130b086b917ed5
woojung yang <yangfriendship@byulsoft.com>
2022-09-11 16:06:07 +0900

    build type jar -> war

--- src/main/java/com/example/ubuntutestserver/controller/MainController.java
+++ src/main/java/com/example/ubuntutestserver/controller/MainController.java
@@ -1,9 +1,9 @@
 package com.example.ubuntutestserver.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.mail.MailProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -23,6 +23,13 @@ public class MainController {
         } catch (InterruptedException e) {
             return ResponseEntity.badRequest().body(result);
         }
+    }
+
+    @GetMapping("/echo")
+    public ResponseEntity<Map<String, Object>> echo(@RequestParam("message") String message) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("message", message == null ? "HI~" : message);
+        return ResponseEntity.ok(result);
     }
 
 }
